a5dbb529e41aa61477b6746258b7d5f4623f0ac0
Edmund Bates <lutemaster@gmail.com>
2012-09-27 23:48:46 -0400

    Pushed live, everything works except that I forgot to specify a default route.

--- AmbientCollage/App_Start/RouteConfig.cs
+++ AmbientCollage/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace AmbientCollage
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "HomePage",
+                url: "",
+                defaults: new { controller = "Login", action = "Greet", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
